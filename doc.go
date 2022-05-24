23b38dfbe70db627e5e79a94403bc913a9d63e22
Marcelo Alves <marcelo.alves@pluto.tv>
2022-05-24 12:01:52 +0000

    init: :rocket:

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package re2postfix provides an algorithm to rewrite infix regular expressions
+// like “a(bb)+a” into equivalent postfix expressions (subset of the traditional
+// Unix egrep regular expression syntax).
+package re2postfix
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/celicoo/re2postfix
+
+go 1.18
+
+require github.com/celicoo/ttest v0.0.0-20220220135518-7bb6907ab1ce // indirect
